63074aecb495b404c8c22e5589678c216946038b
Matt Mangum <mtmangum@utexas.edu>
2014-05-29 10:51:11 -0500

    Added another example page

---
+++ app/scripts/datay.js
@@ -0,0 +1,31 @@
+var DATASET_TITLE = "Sales of Distillate Fuel Oil ";
+var DATASET = [
+	{
+		"SID": "PET.KD0VRSNUS1.A",
+		"title": "Residential",
+		"color": "darkblue "
+	},
+	{
+		"SID": "PET.KD0VCSNUS1.A",
+		"title": "Commercial",
+		"color": "chocolate"
+	},
+	{
+		"SID": "PET.KD0VISNUS1.A",
+		"title": "Industrial",
+		"color": "green"
+	}	
+];
+var Y_VALUE_LIST = [0, 2000000, 4000000, 6000000, 8000000, 10000000];
+
+var CHART_WIDTH = 500;
+var CHART_HEIGHT = 320;
+
+var CHART_PADDING_LEFT = 70;
+var CHART_PADDING_TOP = 40;
+var CHART_PADDING_RIGHT = 10;	// not used
+var CHART_PADDING_BOTTOM = 15;	// not used
+
+// caculate carefully!!!
+var CHART_INNER_WIDTH = 420;
+var CHART_INNER_HEIGHT = 250;
