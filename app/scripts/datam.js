a31ceba18cd7d992fa5098a8e418da0bbccbde69
Matt Mangum <mtmangum@utexas.edu>
2014-05-29 10:40:58 -0500

    Added feature for multiple lines

---
+++ app/scripts/datam.js
@@ -0,0 +1,35 @@
+var DATASET_TITLE = "";
+var DATASET = [
+	{
+		"SID": "ELEC.GEN.ALL-US-99.M",
+		"title": "All",
+		"color": "darkblue"
+	},
+	{
+		"SID": "ELEC.GEN.COW-US-99.M",
+		"title": "Coal",
+		"color": "chocolate"
+	},
+	{
+		"SID": "ELEC.GEN.NUC-US-99.M",
+		"title": "Nuclear",
+		"color": "orange"
+	},
+	{
+		"SID": "ELEC.GEN.NG-US-99.M",
+		"title": "Natural Gas",
+		"color": "green"
+	}	
+];
+var Y_VALUE_LIST = [0, 100000, 200000, 300000, 400000, 500000];
+
+var CHART_WIDTH = 540;
+var CHART_HEIGHT = 320;
+
+var CHART_PADDING_LEFT = 60;
+var CHART_PADDING_TOP = 10;
+
+// caculate carefully!!!
+var CHART_INNER_WIDTH = 480;
+var CHART_INNER_HEIGHT = 250;
+  
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,14 @@
 
 /* Should this go in html? */
-
+// json object to 
+//var DATA_FREQ = "M";	// Currently support A - annual, M - monthly.
 
 
 // -----------------------  DO NOT EDIT BELOW ----------------------------------
 
 var API_KEY = "0FB373836A1B16D4B0700CCCEC3C1845";
-var dataUrl = "http://api.eia.gov/series/?api_key="+API_KEY+"&series_id="+SERIES_ID;
-var CHART_WIDTH = 535;
-var CHART_HEIGHT = 300;
-var CANVAS_WIDTH = 545;
-var CANVAS_HEIGHT = 325;
-
-var CHART_PADDING_LEFT = 60;
-var CHART_PADDING_RIGHT = 10;
-var CHART_PADDING_TOP = 30;
-var CHART_PADDING_BOTTOM = 15;
-
-// caculate carefully!!!
-var CHART_INNER_WIDTH = 510;
-var CHART_INNER_HEIGHT = 255;
+//var dataUrl = "http://api.eia.gov/series/?api_key="+API_KEY+"&series_id="+SERIES_ID;
+var dataUrl = "http://api.eia.gov/series/?api_key="+API_KEY+"&series_id=";
 
 var CHART_GRID_AXIS_WIDTH = 1;
 var CHART_GRID_AXIS_COLOR = "black";
@@ -29,8 +18,6 @@ var CHART_GRID_LINE_COLOR = "black";
 var CHART_GRID_FONT_STYLE = '12px Helvetica, Arial';
 var CHART_GRID_FONT_COLOR = "#555";
 
-var CHART_REFLINE_WIDTH = 1;
-var CHART_REFLINE_COLOR = "gray";
 var CHART_REFNODE_RADIUS = 5;
 var CHART_REFNODE_COLOR = "#813600";
 
@@ -60,7 +47,7 @@ var CHART_TITLE_FONT_SIZE = "16px";
 var CHART_UNIT_FONT_STYLE = '8px Helvetica, Arial';
 var CHART_UNIT_FONT_COLOR = "grey";
 
-var CHART_LINE_WIDTH = 4;
+var CHART_LINE_WIDTH = 3;
 var CHART_LINE_COLOR = "#d35e13";
 
 var CHART_FREQ = "M";
@@ -83,13 +70,15 @@ var CHART_UPDATED = "last updated";
 (function ($) {
 $(document).ready(function() {
 	var refLine = null, refNode = null, refDialog = null, refBound = null;
-	var dataList = new Array();
+	var dataSetList = new Array(), dataList = new Array();
 
 	// create canvas
-	var r = Raphael(document.getElementById("canvas"), CANVAS_WIDTH, CANVAS_HEIGHT);	
+	var r = Raphael(document.getElementById("canvas"), CHART_WIDTH, CHART_HEIGHT);	
 
-	// create connection
+	// create via the first data
 	var xhr = new XMLHttpRequest();
+	var firstUrl = dataUrl + DATASET[0].SID;
+	xhr.open('GET', firstUrl, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             // collect data
@@ -106,100 +95,155 @@ $(document).ready(function() {
 
 				CHART_COL_COUNT = (CHART_END_YEAR-CHART_START_YEAR)*12+CHART_END_MONTH-1;
 				CHART_COL_WIDTH = Math.floor(CHART_INNER_WIDTH/CHART_COL_COUNT);
+			} else {
+				CHART_START_YEAR = Math.round(Number(chartStart.substr(0,4)));
+				CHART_END_YEAR = Math.round(Number(chartEnd.substr(0,4)));
+
+				CHART_COL_COUNT = CHART_END_YEAR-CHART_START_YEAR;
+				CHART_COL_WIDTH = Math.floor(CHART_INNER_WIDTH/CHART_COL_COUNT);
 			}
 
-			CHART_TITLE = response["series"][0]["name"];
+			CHART_INNER_WIDTH = Math.round(CHART_COL_WIDTH*CHART_COL_COUNT);
+
 			CHART_UNIT = response["series"][0]["units"];
-			CHART_UNIT = fixUnits(CHART_UNIT);
-			CHART_DESCRIPTION = response["series"][0]["description"];
-			CHART_SOURCE = response["series"][0]["source"];
-			CHART_UPDATED = response["series"][0]["updated"];
+			// CHART_TITLE = response["series"][0]["name"];
+			// CHART_DESCRIPTION = response["series"][0]["description"];
+			// CHART_SOURCE = response["series"][0]["source"];
+			// CHART_UPDATED = response["series"][0]["updated"];
 
-			var chartData = response["series"][0]["data"];
-
-			// process data list
-			for(var i=0; i<chartData.length; i++) {
-				var pCoords = getCoords(chartData[i][0], chartData[i][1]);
-				chartData[i][1] = Math.round(chartData[i][1]*100)/100; // round to 2 decimals
-				chartData[i][1] = commaSeparateNumber(chartData[i][1]); // comma separate
-				// console.log(convertToMonth(pCoords.month));
-				pCoords.month = convertToMonth(pCoords.month); // set month string value (ex: 1 = January)
-				dataList.push({id:i, year:pCoords.year, month:pCoords.month, x:pCoords.x, y:pCoords.y, xdata:chartData[i][0], ydata:chartData[i][1]});
-			}
-
-			// draw title and units
-			r.text(5, 10, CHART_TITLE).attr({"font":CHART_TITLE_FONT_STYLE, "font-size":CHART_TITLE_FONT_SIZE, "fill":CHART_TITLE_FONT_COLOR, "text-anchor":"start"});
-			//r.text(0, 40, CHART_UNIT).attr({"font":CHART_UNIT_FONT_STYLE, "fill":CHART_UNIT_FONT_COLOR, "text-anchor":"start"});
-
-			// draw chart
+			// draw grid
 			r.drawGrid();
-			r.drawChart(dataList);
-
-			//draw reference line
-			refLine = r.path("M "+CHART_PADDING_LEFT+" "+CHART_PADDING_TOP+" v "+CHART_INNER_HEIGHT)
-						.attr({"stroke-width":CHART_REFLINE_WIDTH, "stroke":CHART_REFLINE_COLOR})
-						.hide();
-			//draw reference node
-			refNode = r.circle(dataList[dataList.length-1].x, dataList[dataList.length-1].y, CHART_REFNODE_RADIUS)
-						.attr({"stroke-width":0,"fill":CHART_REFNODE_COLOR})
-						.hide();
-			//draw reference dialog
-			refDialog = r.set();	// [0]:rectangle, [1] line 1, [2] line 2, [3] line 3			
-			refDialog.push(r.rect(0, 0, CHART_REFDG_WIDTH, CHART_REFDG_HEIGHT, CHART_REFDG_CORNER).attr({"stroke-width":CHART_REFDG_STROKE_WIDTH,"stroke":CHART_REFDG_STROKE,"fill":CHART_REFDG_FILL,"fill-opacity":CHART_REFDG_FILL_OP}));
-			refDialog.push(r.text(10, 10, CHART_START_YEAR+"-"+CHART_START_MONTH).attr({"font":CHART_REFDG_FONT_STYLE,"font":CHART_REFDG_FONT_STYLE,"fill":CHART_REFDG_FONT_COLOR,"text-anchor":"start"}));
-			refDialog.push(r.text(10, 25, "data").attr({"font":CHART_REFDG_DATA_FONT_STYLE,"font-size":CHART_REFDG_FONT_SIZE,"font-weight":"bold","fill":CHART_REFDG_DATA_FONT_COLOR,"text-anchor":"start"}));
-			//refDialog.push(r.text(10, 40, CHART_UNIT).attr({"font":CHART_REFDG_FONT_STYLE,"fill":CHART_REFDG_FONT_COLOR,"text-anchor":"start"}));
-
-			refDialog.hide();
-
-			// draw bounding box for mouse event listener
+			// draw reference bounding box
 			refBound = r.rect(CHART_PADDING_LEFT, CHART_PADDING_TOP, CHART_INNER_WIDTH, CHART_INNER_HEIGHT)
 				.attr({"stroke-width":0, "fill":"yellow", "fill-opacity":0})
-				.hover(handleChartMouseIn, handleChartMouseOut)
-				.mousemove(handleChartMouseMove);
+				.mouseout(handleChartMouseOut);
+			//draw reference node
+			refNode = r.circle(CHART_PADDING_LEFT, CHART_PADDING_TOP, CHART_REFNODE_RADIUS)
+						.attr({"stroke-width":1,"stroke":"white","fill":CHART_REFNODE_COLOR})
+						.hide();
+			//draw reference dialog
+			refDialog = r.set();	// [0]:rectangle, [1] line 1 title+time, [2] line 2 data		
+			refDialog.push(r.rect(0, 0, CHART_REFDG_WIDTH, CHART_REFDG_HEIGHT, CHART_REFDG_CORNER).attr({"stroke-width":CHART_REFDG_STROKE_WIDTH,"stroke":CHART_REFDG_STROKE,"fill":CHART_REFDG_FILL,"fill-opacity":CHART_REFDG_FILL_OP}));
+			refDialog.push(r.text(10, 10, "title"+": "+CHART_START_YEAR+"-"+CHART_START_MONTH).attr({"font":CHART_REFDG_FONT_STYLE,"font":CHART_REFDG_FONT_STYLE,"fill":CHART_REFDG_FONT_COLOR,"text-anchor":"start"}));
+			refDialog.push(r.text(10, 25, "data").attr({"font":CHART_REFDG_DATA_FONT_STYLE,"font-size":CHART_REFDG_FONT_SIZE,"font-weight":"bold","fill":CHART_REFDG_DATA_FONT_COLOR,"text-anchor":"start"}));
+			refDialog.hide();
+			// draw dataset title
+			r.text(0, 10, DATASET_TITLE).attr({"font":CHART_TITLE_FONT_STYLE, "font-size":CHART_TITLE_FONT_SIZE, "fill":CHART_TITLE_FONT_COLOR, "text-anchor":"start"});
 
-        }
-    }
-	xhr.open('GET', dataUrl, true);
+			// get first data
+			var chartData = response["series"][0]["data"];
+			dataSetList[0] = response["series"][0]["data"];
+			r.drawChart(0);
+			// get remaining data
+			var dataFlagList = new Array();
+			dataFlagList[0] = true;
+			for(var i=1; i<DATASET.length; i++) {
+				dataFlagList[i] = false;
+				getMoreData(i);
+			}
+			function getMoreData(idx) {
+				var ixhr = new XMLHttpRequest();
+				var iUrl = dataUrl + DATASET[idx].SID;
+				ixhr.open('GET', iUrl, true);
+				ixhr.onreadystatechange = function() {
+					if (ixhr.readyState == 4 && ixhr.status == 200) {
+						var iresponse = JSON.parse(ixhr.response);
+						dataSetList[idx] = iresponse["series"][0]["data"];
+
+						r.drawChart(idx);
+					}
+				}
+				ixhr.send(); 
+			}
+        }	// end if status
+    }	// end onreadystatechange
 	xhr.send(); 		
 
-	Raphael.fn.drawGrid = function() {
+	Raphael.fn.drawGrid = function() {		
 		// x-lines
-		if(CHART_FREQ=="M") {
-			var currYear = CHART_START_YEAR;
-			for(var i=0; i<=CHART_COL_COUNT; i=i+12) {
-				this.path("M "+(CHART_PADDING_LEFT+i*CHART_COL_WIDTH)+" "+(CHART_HEIGHT-CHART_PADDING_BOTTOM)+" v 10")
+		var currYear = CHART_START_YEAR;	
+		if(CHART_FREQ=="M") {	// monthly
+			var step = 1;
+			if(CHART_COL_COUNT<=120) {
+				step = 1;
+			} else if(CHART_COL_COUNT<=240) {
+				step = 2;
+			} else {
+				step = 5;
+			}			
+			for(var i=0; i<=CHART_COL_COUNT; i=i+12*step) {
+				this.path("M "+(CHART_PADDING_LEFT+i*CHART_COL_WIDTH)+" "+(CHART_PADDING_TOP+CHART_INNER_HEIGHT)+" v 10")
 					.attr({"stroke-width":CHART_GRID_LINE_WIDTH, "stroke":CHART_GRID_LINE_COLOR});
-				this.text((CHART_PADDING_LEFT+i*CHART_COL_WIDTH), (CHART_HEIGHT-CHART_PADDING_BOTTOM), currYear)
+				this.text((CHART_PADDING_LEFT+i*CHART_COL_WIDTH), (CHART_PADDING_TOP+CHART_INNER_HEIGHT), currYear)
 					.attr({"font":CHART_GRID_FONT_STYLE, "fill":CHART_GRID_FONT_COLOR})
 					.transform("T0,20");
-				currYear++;
+				currYear=currYear+step;
+			}
+		} else {	// annually
+			var step = 1;
+			if(CHART_COL_COUNT<=10) {
+				step = 1;
+			} else if(CHART_COL_COUNT<=20) {
+				step = 2;
+			} else if(CHART_COL_COUNT<=50) {
+				step = 5;
+			} else {
+				step = 10;
+			}			
+			for(var i=0; i<=CHART_COL_COUNT; i=i+step) {
+				this.path("M "+(CHART_PADDING_LEFT+i*CHART_COL_WIDTH)+" "+(CHART_PADDING_TOP+CHART_INNER_HEIGHT)+" v 10")
+					.attr({"stroke-width":CHART_GRID_LINE_WIDTH, "stroke":CHART_GRID_LINE_COLOR});
+				this.text((CHART_PADDING_LEFT+i*CHART_COL_WIDTH), (CHART_PADDING_TOP+CHART_INNER_HEIGHT), currYear)
+					.attr({"font":CHART_GRID_FONT_STYLE, "fill":CHART_GRID_FONT_COLOR})
+					.transform("T0,20");
+				currYear=currYear+step;
 			}
 		}
+
 		// x-axis
-		this.path("M "+CHART_PADDING_LEFT+" "+(CHART_HEIGHT-CHART_PADDING_BOTTOM)+" h "+CHART_INNER_WIDTH)
+		this.path("M "+CHART_PADDING_LEFT+" "+(CHART_PADDING_TOP+CHART_INNER_HEIGHT)+" h "+CHART_INNER_WIDTH)
 			.attr({"stroke-width":CHART_GRID_AXIS_WIDTH, "stroke":CHART_GRID_AXIS_COLOR});
-		this.text(CHART_PADDING_LEFT, (CHART_HEIGHT-CHART_PADDING_BOTTOM), numToString(Y_VALUE_LIST[0]))
+		this.text(CHART_PADDING_LEFT, (CHART_PADDING_TOP+CHART_INNER_HEIGHT), numToString(Y_VALUE_LIST[0]))
 			.attr({"font":CHART_GRID_FONT_STYLE, "fill":CHART_GRID_FONT_COLOR, "text-anchor":"end"})
 			.transform("T-10,0");
 		// y-lines
 		for(var i=1; i<=CHART_ROW_COUNT; i++) {
-			this.path("M "+CHART_PADDING_LEFT+" "+(CHART_HEIGHT-CHART_PADDING_BOTTOM-i*CHART_ROW_HEIGHT)+" h "+CHART_INNER_WIDTH)
+			this.path("M "+CHART_PADDING_LEFT+" "+(CHART_PADDING_TOP+CHART_INNER_HEIGHT-i*CHART_ROW_HEIGHT)+" h "+CHART_INNER_WIDTH)
 				.attr({"stroke-width":CHART_GRID_LINE_WIDTH, "stroke":CHART_GRID_LINE_COLOR});
-			this.text(CHART_PADDING_LEFT, (CHART_HEIGHT-CHART_PADDING_BOTTOM-i*CHART_ROW_HEIGHT), numToString(Y_VALUE_LIST[i]))
+			this.text(CHART_PADDING_LEFT, (CHART_PADDING_TOP+CHART_INNER_HEIGHT-i*CHART_ROW_HEIGHT), numToString(Y_VALUE_LIST[i]))
 				.attr({"font":CHART_GRID_FONT_STYLE, "fill":CHART_GRID_FONT_COLOR, "text-anchor":"end"})
 				.transform("T-10,0");
 		}
 	};	// end drawGrid
 
-	Raphael.fn.drawChart = function() {
-		var pathStr = "M "+dataList[0].x+" "+dataList[0].y+" ";
-    	for(var i=1; i<dataList.length; i++) {
-    		//this.circle(dataList[j].x, dataList[j].y, CHART_NODE_RADIUS).attr({"stroke":CHART_NODE_COLOR, "fill":CHART_NODE_COLOR});
-    		pathStr = pathStr+"L "+dataList[i].x+" "+dataList[i].y+" ";
-    	}
-    	this.path(pathStr)
-    		.attr({"stroke-width":CHART_LINE_WIDTH, "stroke":CHART_LINE_COLOR, "stroke-linejoin":"miter",'stroke-linecap': "round"});
+	Raphael.fn.drawChart = function(cid) {
+		CHART_LINE_COLOR = DATASET[cid].color;
+		var pathStr = "";
+		// populate dataList (global)
+		dataList[cid] = new Array();			
+		for(var i=0; i<dataSetList[cid].length; i++) {
+			var pt = getCoords(dataSetList[cid][i][0], dataSetList[cid][i][1]);	// time, value
+			dataList[cid].push({
+				id:i, 
+				year:pt.year, 
+				month:pt.month, 
+				x:pt.x, 
+				y:pt.y, 
+				xdata:dataSetList[cid][i][0], 
+				ydata:dataSetList[cid][i][1]
+			});
+
+			if(i==0) {
+				pathStr = "M "+pt.x+" "+pt.y+" ";
+			} else {
+				pathStr = pathStr+"L "+pt.x+" "+pt.y+" ";
+			}
+		}
+
+		this.path(pathStr)
+			.data("cid", cid)
+			.attr({"stroke-width":CHART_LINE_WIDTH, "stroke":CHART_LINE_COLOR, "stroke-linejoin":"round",'stroke-linecap': "round"})
+			.mouseover(handleChartMouseIn).mousemove(handleChartMouseIn);
     }
 
 	// event handlers
@@ -207,79 +251,79 @@ $(document).ready(function() {
 		evt.preventDefault();
 		evt.stopPropagation();
 
-		refLine.show();
-		refNode.show();
-		refDialog.show();
-
-	}
-	function handleChartMouseOut(evt) {
-		evt.preventDefault();
-		evt.stopPropagation();		
-		
-		refLine.hide();
-		refNode.hide();
-		refDialog.hide();
-	}
-	function handleChartMouseMove(evt) {
-		evt.preventDefault();
-		evt.stopPropagation();
+		var thisDataList = dataList[this.data("cid")];
 
 		var mouseX = evt.clientX-this.node.parentNode.offsetLeft;
 		var px = 0, py=0, dataid=0;
 		if(mouseX<=CHART_PADDING_LEFT) {
 			px = CHART_PADDING_LEFT;
-			py = dataList[dataList.length-1].y;
-			dataid = dataList.length-1;
-		} else if( mouseX>=dataList[0].x ){
-			px = dataList[0].x;
-			py = dataList[0].y;
+			py = thisDataList[thisDataList.length-1].y;
+			dataid = thisDataList.length-1;
+		} else if( mouseX>=thisDataList[0].x ){
+			px = thisDataList[0].x;
+			py = thisDataList[0].y;
 			dataid = 0;
 		} else {
-			for(var i=0; i<dataList.length-1; i++) {
-				if(dataList[i+1].x<=mouseX && mouseX<=dataList[i].x) {
-					if( (mouseX-dataList[i+1].x)<=(dataList[i].x-mouseX) ) {
-						px = dataList[i+1].x;
-						py = dataList[i+1].y;
+			for(var i=0; i<thisDataList.length-1; i++) {
+				if(thisDataList[i+1].x<=mouseX && mouseX<=thisDataList[i].x) {
+					if( (mouseX-thisDataList[i+1].x)<=(thisDataList[i].x-mouseX) ) {
+						px = thisDataList[i+1].x;
+						py = thisDataList[i+1].y;
 						dataid = i+1;
 					} else {
-						px = dataList[i].x;
-						py = dataList[i].y;
+						px = thisDataList[i].x;
+						py = thisDataList[i].y;
 						dataid = i;
 					}
 				}
 			}
 		}
 
-		var refPath = "M "+px+" "+CHART_PADDING_TOP+" v "+CHART_INNER_HEIGHT;
-		refLine.attr({"path": refPath});
-
-		refNode.attr({"cx":px, "cy":py});
+		refNode.attr({"cx":px, "cy":py, "fill":DATASET[this.data("cid")].color});
+		refNode.toFront().show();
 
 		var dx = px+CHART_REFDG_OFFX;
 		var dy = py+CHART_REFDG_OFFY;
 		if( (dx+CHART_REFDG_WIDTH)>=(CHART_PADDING_LEFT+CHART_INNER_WIDTH) ) {
 			dx = px-CHART_REFDG_OFFX-CHART_REFDG_WIDTH;
 		}
-		refDialog[1].attr({"text":(dataList[dataid].month+" "+dataList[dataid].year)});
-		refDialog[2].attr({"text":(dataList[dataid].ydata+" "+CHART_UNIT)});
+		var cmonth = monthToString(thisDataList[dataid].month);
+		var cyear = thisDataList[dataid].year;
+		var cdata = numToString(thisDataList[dataid].ydata.toFixed(2));
+		var cunit = convertUnit(CHART_UNIT);
+		if(CHART_FREQ=="M") {
+			refDialog[1].attr({"text":(DATASET[this.data("cid")].title+": "+cmonth+" "+cyear)});
+		} else {
+			refDialog[1].attr({"text":(DATASET[this.data("cid")].title+": "+cyear)});
+		}
+		refDialog[2].attr({"text":(cdata+" "+cunit), "fill":DATASET[this.data("cid")].color});
 		refDialog.transform("T"+dx+","+dy);
+		refDialog.toFront().show();
+	}
+	function handleChartMouseOut(evt) {
+		evt.preventDefault();
+		evt.stopPropagation();		
+		
+		// refLine.hide();
+		refNode.hide();
+		refDialog.hide();
 	}
 
 	// helper functions
 	function getCoords(xval, yval) {
-		var currYear = 0, currMonth=0;
+		var currYear = Math.round(Number(xval.substr(0,4)));
 		var xCoord = 0;
 		if(CHART_FREQ=="M") {
-			var currYear = Math.round(Number(xval.substr(0,4)));
 			var currMonth = Math.round(Number(xval.substr(4,2)));
-			var currCol = (currYear-CHART_START_YEAR)*12+currMonth-1;
 			xCoord = Math.round( ((currYear-CHART_START_YEAR)*12+currMonth-1)*CHART_COL_WIDTH+CHART_PADDING_LEFT );
+		} else {
+			xCoord = Math.round( (currYear-CHART_START_YEAR)*CHART_COL_WIDTH+CHART_PADDING_LEFT );
 		}
 
-		var yCoord = Math.round( CHART_HEIGHT-(yval-Y_VALUE_LIST[0])*CHART_INNER_HEIGHT/(Y_VALUE_LIST[Y_VALUE_LIST.length-1]-Y_VALUE_LIST[0])-CHART_PADDING_BOTTOM );
+		var yCoord = Math.round( CHART_PADDING_TOP+CHART_INNER_HEIGHT*(1-(yval-Y_VALUE_LIST[0])/(Y_VALUE_LIST[Y_VALUE_LIST.length-1]-Y_VALUE_LIST[0])) );
 
 		return {year:currYear, month:currMonth, x:xCoord, y:yCoord};
-	}
+	};
 
 	function numToString(x) {
 		if(x>=1000) {
@@ -289,26 +333,12 @@ $(document).ready(function() {
 		}
 	};
 
-	function commaSeparateNumber(val){
-	    while (/(\d+)(\d{3})/.test(val)){
-	      val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-	    }
-	    return val;
-	}
-
-	function commaSeparateNumber(val){
-	    while (/(\d+)(\d{3})/.test(val)){
-	      val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-	    }
-	    return val;
-	}
-
-	function convertToMonth(int) {
+	function monthToString(int) {
 		var monthArr = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 		return monthArr[int-1];
-	}
+	};
 
-	function fixUnits(unit) {
+	function convertUnit(unit) {
 		var newUnit = "";
 		switch(unit){
 			case 'thousand megawatthours':
@@ -319,9 +349,7 @@ $(document).ready(function() {
 		}
 
 		return newUnit;
-	}
-
-
+	};
 
 });	// end $(document).ready
 }(jQuery));
